0325deaa68881511f0e8c03c84e16b1baa404b55
tony-tripulca <tony.tripulca@gmail.com>
2024-02-13 14:17:02 +0800

    Coming soon menu

--- src/modules/menu/Menu.js
+++ src/modules/menu/Menu.js
@@ -1,14 +1,67 @@
-import React from 'react';
-import {Text, View} from 'react-native';
+import React, {useCallback, useState} from 'react';
+import {
+  RefreshControl,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+import {SafeAreaView} from 'react-native-safe-area-context';
 
 import {text} from '../../styles/app';
 
 function Menu() {
+  const [refreshing, setRefreshing] = useState(false);
+
+  const onRefresh = useCallback(() => {
+    setRefreshing(true);
+    setTimeout(() => {
+      setRefreshing(false);
+    }, 2000);
+  }, []);
+
   return (
-    <View>
-      <Text style={[text.title, text.color.black]}>Menu</Text>
-    </View>
+    <SafeAreaView style={styles.view.safeArea}>
+      <ScrollView
+        contentContainerStyle={styles.view.scroll}
+        refreshControl={
+          <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+        }>
+        <Text style={[text.title, text.color.black]}>Menu</Text>
+        <View style={styles.view.card.container}>
+          <Text style={[text.h1, text.color.black]}>Coming soon...</Text>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  view: {
+    safeArea: {
+      flex: 1,
+      paddingTop: StatusBar.currentHeight,
+    },
+    scroll: {
+      marginHorizontal: 20,
+    },
+    card: {
+      container: {
+        marginBottom: 16,
+        padding: 16,
+        borderWidth: 1,
+        borderRadius: 15,
+      },
+    },
+  },
+  MenuImage: {
+    marginBottom: 16,
+    borderRadius: 100,
+    width: 128,
+    height: 128,
+    resizeMode: 'contain',
+  },
+});
 
 export default Menu;
